868dadedaf33ffb00d39bd40abd433ab648ff4dc
PolitoCarlotta <carlottapolitowd@gmail.com>
2025-05-30 11:24:34 +0200

    corretto funzioni

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,27 +1,27 @@
 // inserire una parola
 // funzione per capire se palindroma
     let name= prompt('Inserisci una parola').toLowerCase()
-    let result=false
-    let reversedWord=''
 
 function reverse (word){
+    let reversedWord=''
     for(let i=word.length -1; i>=0; i--){
         reversedWord += word[i]
+        }
+    return reversedWord
     }
-}
 
-reverse(name)
 
-function isPalindroma (word1,word2){
-    if(reversedWord=== name){
-        return result = true
+function isPalindroma (word){
+    const reversedWord= reverse(word)
+    if(reversedWord=== word){
+        return true
     }
     else{
-        return result
+        return false
     }
 }
 
-const declaration= isPalindroma(name,reversedWord)
+const declaration= isPalindroma(name)
 
 if(declaration === true){
     console.log(`la parola ${name} è palindroma` )
@@ -29,7 +29,7 @@ if(declaration === true){
 else{
     console.log(`la parola ${name} non è palindroma`)
 }
-console.log(name, reversedWord, result)
+
 
 
 
